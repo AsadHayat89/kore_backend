aed09e0dace83160d550eafa08cfca2c842f7cae
Asad Hayat <asad.hayat@palmgrid.com>
2024-07-04 23:05:53 +0500

    for testing geocoding

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Import required modules
-const routes=require('./routes/index');
+const routes = require('./routes/index');
+const axios = require('axios');
 require('dotenv').config(); // Load environment variables from a .env file
 const express = require('express'); // Import the Express.js framework
 //const routes = require('./routes'); // Import the routes module
@@ -22,10 +23,26 @@ const bootstrap = async () => {
     app.set('trust proxy', true);
 
     // Use the main router for handling routes
-   app.use(routes);
+    app.use(routes);
 
+    app.get('/reverse-geocode', async (req, res) => {
+        const { lat, lon, apiKey } = req.query;
 
-    
+        if (!lat || !lon || !apiKey) {
+            return res.status(400).send('Missing required query parameters: lat, lon, apiKey');
+        }
+
+        const url = `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=${lat}%2C${lon}%2C250&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=${apiKey}`;
+
+        try {
+            const response = await axios.get(url);
+            res.json(response.data);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('An error occurred while fetching the reverse geocoding data');
+        }
+    });
+
     // Handle unexpected router hits by returning a 404 error
     app.all('*', (req, res, next) => {
         next(
